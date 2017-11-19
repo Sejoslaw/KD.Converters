430ab3fe908e3a477acd04ab235deecc61a1e5b5
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-11-19 16:59:13 +0100

    Added ToJson conversion.

---
+++ KD.Converters/Json/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace KD.Converters.Json
+{
+    /// <summary>
+    /// Extension methods used to convert to / from JSON.
+    /// </summary>
+    public static class Extensions
+    {
+        public static string ToJson<TSource>(this TSource source)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TSource));
+            using (MemoryStream stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, source);
+                stream.Position = 0;
+                StreamReader reader = new StreamReader(stream);
+                string json = reader.ReadToEnd();
+                return json;
+            }
+        }
+    }
+}
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using KD.Converters.Json;
+using System;
+using System.Runtime.Serialization;
 
 namespace Tests
 {
+    [DataContract]
+    class Person
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Person p = new Person { Id = 1, Name = "Krzysztof" };
+            string json = p.ToJson();
+            Console.WriteLine(json);
+
             Console.ReadKey();
         }
     }
